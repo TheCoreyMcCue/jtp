4e9dec60f20fbafa5395eef9557401e950bcf300
Corey McCue <corey.mccue@sky.uk>
2022-10-29 13:53:34 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { generateWordDocument } from './wordDocument';
+
+import Button from '@mui/material/Button';
+import { FormControl, TextField } from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [customerName, setCustomerName] = useState('');
+	const [latency, setLatency] = useState('');
+	const [throughput, setThroughput] = useState('');
+	const [geographies, setGeographies] = useState('');
+	const [analysisMethods, setAnalysisMethods] = useState('');
+
+	var today = new Date();
+	var dd = String(today.getDate()).padStart(2, '0');
+	var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+	var yyyy = today.getFullYear();
+	today = yyyy + '-' + mm + '-' + dd;
+
+	const onCustomerChange = (e) => setCustomerName(e.target.value);
+	const onLatencyChange = (e) => setLatency(e.target.value);
+	const onThroughputChange = (e) => setThroughput(e.target.value);
+	const ongeographiesChange = (e) => setGeographies(e.target.value);
+	const onanalysisMethodsChange = (e) => setAnalysisMethods(e.target.value);
+
+	return (
+		<div>
+			<section>
+				<h1 style={{ textAlign: 'center' }}>Joint Test Plan</h1>
+				<FormControl
+					style={{
+						display: 'flex',
+						width: '30%',
+						justifyContent: 'center',
+						margin: 'auto',
+					}}
+				>
+					<TextField
+						id='outlined-basic'
+						label='Customer Name'
+						variant='outlined'
+						onChange={onCustomerChange}
+					/>
+					<h3>Metrics & Analysis</h3>
+					<TextField
+						id='outlined-basic'
+						label='Latency Requirement (p99)'
+						variant='outlined'
+						onChange={onLatencyChange}
+					/>
+					<TextField
+						id='outlined-basic'
+						label='Throughput Requirement (QPS)'
+						variant='outlined'
+						onChange={onThroughputChange}
+						sx={{ marginTop: '10px' }}
+					/>
+					<TextField
+						id='outlined-basic'
+						label='Geographies (only if relevant to the test)'
+						variant='outlined'
+						onChange={ongeographiesChange}
+						sx={{ marginTop: '10px' }}
+					/>
+					<TextField
+						id='outlined-basic'
+						label='Analysis Method (Rules or Machine Learning?)'
+						variant='outlined'
+						onChange={onanalysisMethodsChange}
+						sx={{ marginTop: '10px' }}
+					/>
+				</FormControl>
+			</section>
+			<section
+				style={{
+					display: 'flex',
+					width: '30%',
+					justifyContent: 'center',
+					margin: 'auto',
+				}}
+			>
+				<Button
+					onClick={() =>
+						generateWordDocument(
+							customerName,
+							latency,
+							throughput,
+							geographies,
+							analysisMethods
+						)
+					}
+					variant='contained'
+					style={{ marginTop: '10px' }}
+				>
+					Generate Form
+				</Button>
+			</section>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/wordDocument.js
@@ -0,0 +1,156 @@
+import {
+	AlignmentType,
+	Document,
+	HeadingLevel,
+	Packer,
+	Paragraph,
+	TextRun,
+} from 'docx';
+import { saveAs } from 'file-saver';
+
+export const generateWordDocument = async (
+	customer,
+	latency,
+	throughput,
+	geographies,
+	analysisMethod
+) => {
+	let doc = new Document({
+		sections: [
+			{
+				children: [
+					new Paragraph({
+						heading: HeadingLevel.HEADING_1,
+						alignment: AlignmentType.CENTER,
+						children: [
+							new TextRun({
+								text: `Joint Test Plan - ${customer}`,
+								bold: true,
+								font: 'arial',
+								color: '#000000',
+								size: 40,
+							}),
+						],
+					}),
+					new Paragraph({
+						heading: HeadingLevel.HEADING_3,
+						// alignment: AlignmentType.CENTER,
+						children: [
+							new TextRun({
+								text: 'Objective Statement',
+								break: 2,
+								bold: true,
+								font: 'arial',
+								color: '#000000',
+								size: 28,
+							}),
+							new TextRun({
+								break: 2,
+							}),
+						],
+					}),
+					new Paragraph({
+						border: {
+							top: {
+								color: '#000000',
+								space: 4,
+								style: 'single',
+								size: 6,
+							},
+							bottom: {
+								color: '#000000',
+								space: 4,
+								style: 'single',
+								size: 6,
+							},
+							left: {
+								color: '#000000',
+								space: 4,
+								style: 'single',
+								size: 6,
+							},
+							right: {
+								color: '#000000',
+								space: 4,
+								style: 'single',
+								size: 6,
+							},
+						},
+						children: [
+							new TextRun({
+								text: `Offline historical test to evaluate Ekata’s Address Risk API’s features in a modeling exercise to determine if Ekata can help improve ${customer}’s ability to detect fraud by {X}%.`,
+								font: 'arial',
+								color: '#000000',
+								size: 20,
+							}),
+						],
+					}),
+					new Paragraph({
+						children: [
+							new TextRun({
+								text: 'Metrics & Analysis',
+								break: 1,
+								font: 'arial',
+								color: '#999999',
+								size: 28,
+							}),
+						],
+					}),
+					new Paragraph({
+						bullet: { level: 0 },
+						break: 1,
+						children: [
+							new TextRun({
+								text: `Latency Requirement (p99): ${latency}`,
+								bold: true,
+								font: 'arial',
+								color: '#000000',
+								size: 20,
+							}),
+						],
+					}),
+					new Paragraph({
+						bullet: { level: 0 },
+						children: [
+							new TextRun({
+								text: `Throughput Requirement (QPS): ${throughput}`,
+								bold: true,
+								font: 'arial',
+								color: '#000000',
+								size: 20,
+							}),
+						],
+					}),
+					new Paragraph({
+						bullet: { level: 0 },
+						children: [
+							new TextRun({
+								text: `Geographies: ${geographies}`,
+								bold: true,
+								font: 'arial',
+								color: '#000000',
+								size: 20,
+							}),
+						],
+					}),
+					new Paragraph({
+						bullet: { level: 0 },
+						children: [
+							new TextRun({
+								text: `Analysis Method: ${analysisMethod}`,
+								bold: true,
+								font: 'arial',
+								color: '#000000',
+								size: 20,
+							}),
+						],
+					}),
+				],
+			},
+		],
+	});
+	Packer.toBlob(doc).then((blob) => {
+		// saveAs from FileSaver will download the file
+		saveAs(blob, `${customer} - JTP.docx`);
+	});
+};
